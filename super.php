482e4f9314da3d656575a8acae1ee9009d3d1a61
vaix1n <93144206+vaix1n@users.noreply.github.com>
2021-10-25 22:23:00 +0400

    Update super.php

--- super.php
+++ super.php
@@ -2,14 +2,14 @@
 		    array(
 		        'apiKey' => 'QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb',
 		        'site' => 'test',
-		        'order' => array(
+		        'order' => json_encode(array(
 		        	'number' => '7111998', 
 		        	'lastName' => 'Nuzhdaev', 
 		        	'firstName' => 'Dmitry', 
 		        	'patronymic' => 'Sergeevich', 
 		        	'orderType' => 'fizik', 
 		        	'orderMethod' => 'test',
-		        	'customerComment' => 'GITHUB', 
+		        	'customerComment' => 'https://github.com/vaix1n/superposuda/', 
 		        	'status' => 'trouble',
 		        	'items' => array(
 		        		'0' => array(
@@ -22,16 +22,18 @@
 		        		)
 		        	)
 		        	
-		        )
+		        ))
 
 		    );
 
        $ch = curl_init('https://superposuda.retailcrm.ru/api/v5/orders/create'); 
 
+       $json_post = http_build_query($postdata);
+
        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded')); 
        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
        curl_setopt($ch, CURLOPT_POST, 1); 
-       curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata); 
+       curl_setopt($ch, CURLOPT_POSTFIELDS, $json_post); 
        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
        
        $result = curl_exec($ch); 
